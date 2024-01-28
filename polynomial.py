8392d447496ba05743cd6a1e99cab0e1e83f4e93
Tristan Lee <tristanlee73@gmail.com>
2024-01-28 17:32:09 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -5,35 +5,72 @@ class X:
     def __repr__(self):
         return "X"
 
+
 class Int:
     def __init__(self, i):
         self.i = i
-    
+
     def __repr__(self):
         return str(self.i)
+
+
+def parentheses_term(p):
+    return not (isinstance(p, X) or isinstance(p, Int))
+
 
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        if parentheses_term(self.p2):
+            return repr(self.p1) + " - ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " - " + repr(self.p2)
+
 
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+        if parentheses_term(self.p1):
+            if parentheses_term(self.p2):
+                return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
+        if parentheses_term(self.p2):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        if parentheses_term(self.p1):
+            if parentheses_term(self.p2):
+                return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        if parentheses_term(self.p2):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " / " + repr(self.p2)
+
+
+poly = Add(Add(Int(4), Int(3)), Add(
+    X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
 print(poly)
+
+poly2 = Div(Mul(X(), Mul(Add(X(), Int(4)), Sub(X(), Int(4)))), Sub(X(), Mul(Int(2), X())))
+print(poly2)
